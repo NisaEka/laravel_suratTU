a55801b8de860d45ec1813255459f9bccbb415e7
nisa eka <nisa.eka.juliana@gmail.com>
2019-05-15 18:58:28 +0700

    ubah halaman awal

--- resources/themes/default/pages/home.blade.php
+++ resources/themes/default/pages/home.blade.php
@@ -1,66 +1,105 @@
-@extends('cms-frontend::layout.master')
+<!DOCTYPE html>
+<html lang="zxx">
 
-@if (isset($page))
-    @section('seoDescription') {{ $page->seo_description }} @endsection
-    @section('seoKeywords') {{ $page->seo_keywords }} @endsection
-@endif
+<head>
+    <title></title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta charset="utf-8" />
+    
+    <script>
+        addEventListener("load", function() {
+            setTimeout(hideURLbar, 0);
+        }, false);
 
-@section('content')
+        function hideURLbar() {
+            window.scrollTo(0, 1);
+        }
 
-<div class="homepage-banner">
-    <h1>{{ config('app.name') }}</h1>
-    <p class="lead">Fluent, Minimalism</p>
-</div>
+    </script>
+    <!-- Custom Theme files -->
+    <link href="{{asset('css/bootstrap.css')}}" type="text/css" rel="stylesheet" media="all">
+    <link href="{{asset('css/style.css')}}" type="text/css" rel="stylesheet" media="all">
+    <!-- font-awesome icons -->
+    <link href="{{asset('css/font-awesome.min.css')}}" rel="stylesheet">
+    <!-- //Custom Theme files -->
+    <!-- online-fonts -->
+    <link href="//fonts.googleapis.com/css?family=Raleway:100,100i,200,200i,300,300i,400,400i,500,500i,600,600i,700,700i,800" rel="stylesheet">
+    <!-- //online-fonts -->
+</head>
 
-<div class="container">
-
-    @if (isset($page))
-        {!! $page->entry !!}
-    @else
-        <div class="row homepage-container">
-            <div class="col-md-6">
-                <div class="well">
-                    <h3>Basics</h3>
-                    <p>In order to add content to this page login to Cms and add a home page. Remember you can set up
-                        your own Auth for Cms or run the artisan command: <code>php artisan grafite:cms-setup</code> to get
-                        a prebuilt auth system.</p>
-                    <p>Once you're all set up try building a menu with slug: main, and a widget with the slug: widget. You'll see the theme display them right away!</p>
-                </div>
-            </div>
-            <div class="col-md-6">
-                <div class="well">
-                    <h3>Custom Templates</h3>
-                    <p>By default the homepage has its own template but you can add any by following these details:</p>
-                    <p>To create custom templates for different purposes simply make a view in <br>
-                        the <code>resources/themes/{theme-name}/{module-name}</code> directory that looks similar to: `xxxx-template.blade.php`. <br>
-                        This means you still have full control of blade templating but your pages can easily swap out views.</p>
+<body>
+    <section class="main-banner" id="home">
+        <div class="header-top-w3layouts text-right">
+            <ul id="menu">
+                <li>
+                    <input id="check02" type="checkbox" name="menu" />
+                    <label for="check02"><span class="fa fa-bars" aria-hidden="true"></span></label>
+                    <ul class="submenu">
+                        <li><a href="#home">Tentang APPS</a></li>
+                        <li><a href="#about" class="scroll">Fitur & Teknologi</a></li>
+                        @if (Auth::user())
+                        <li><a href="#team" class="scroll">Buka Dashboard</a></li>
+                        <li><a href="{{ url('/logout') }}" class="scroll">Logout</a></li>
+                        @else
+                        <li><a href="#login" class="scroll">Login</a></li>
+                        @endif
+                        
+                    </ul>
+                </li>
+            </ul>
+        </div>
+        <div class="container">
+            <div class="baner-info-w3ls text-left">
+                <h1><a href="index.html">Aplikasi Pengajuan Pembuatan Surat</a></h1>
+                <h6 class="mx-auto mt-4">Aplikasi untuk mengajukan pembuatan surat fakultas sains dan teknologi</h6>
+                <a class="btn btn-primary mt-lg-5 mt-3 agile-link-bnr scroll" href="#about" role="button">Learn More</a>
+                <div class="banner-high-lights text-left">
+                    <div class="rotate">
+                        <a href="#about" class="scroll">
+                               <span class="fa fa-long-arrow-down"></span>                 
+                        </a>
+                    </div>
                 </div>
             </div>
         </div>
-        <div class="row homepage-container">
-            <div class="col-md-6">
-                <div class="well">
-                    <h3>Custom Themes</h3>
-                    <p>You can easily generate a theme template via the command: <code>php artisan theme:generate {name}</code> </p>
-                    <p>The theme's files will be placed in the following directory: <code>resources/themes/{name}</code></p>
-                    <p>To include files either use the blade code: <code>&#64;theme</code> or include a file with the <code>cms-frontend::</code> namespace.</p>
-                </div>
+    </section>
+    <!-- what we do -->
+    <section class="wedo py-lg-5 py-5" id="about">
+        <div class="container py-lg-5">
+            <div class="text-center">
+                <h3 class="tittle_head">Fitur & Teknologi </h3>
             </div>
-            <div class="col-md-6">
-                <div class="well">
-                    <h3>Widgets, Menus, Images &amp; Includes</h3>
-                    <p>Widgets are easy to add to any template since they can be injected with the <code>&#64;menu('slug')</code>, <code>&#64;widget('slug')</code> or <code>&#64;images('tag')</code>. If you don't suppy a tag for the images you will get all images. To include a theme view you can easily use: <code>&#64;theme('path.in.theme')</code>.</p>
-                </div>
-                <br>
+            <div class="wedo_top text-center py-5">
+                <ul>
+                    <li><span class="fa fa-code"></span><h3>Solid Stack</h3>
+                    <p class="text-muted">Menggunakan web frameworks &amp; teknologi yang update.</p></li>
+                    <li><span class="fa fa-dashboard"></span><h3>Mudah Digunakan</h3><p class="text-muted">Sederhana, dengan UI/UX yang slim dan bersih.</p></li>
+                </ul>
             </div>
         </div>
-    @endif
+    </section>
+    <!-- //what we do -->
+    
+    <!-- footer -->
+    <section class="foot-top-w3layouts pt-5">
+        <div class="container py-lg-5">
+            <div class="text-center">
+                <h3 class="tittle_head text-white">Login terlebih dahulu untuk masuk ke sistem </h3>
+            </div>
+            <div class="newsletter-info-wthree text-center pt-md-5 pt-3">
+                <a class="btn btn-primary mt-lg-5 mt-3 agile-link-bnr scroll" href="#login" role="button">Login</a>
+            </div>
 
-</div>
-@endsection
+        </div>
+        <div class="cpy-right text-center pb-5">
+            
+            <p class="copy-w3ls mb-4">© 2019 K.
+            </p>
+            <a href="#home" class="move-top scroll"><span class="fa fa-angle-double-up" aria-hidden="true"></span></a>
+        </div>
+    </section>
+    <!-- //footer -->
 
-@section('cms')
-    @if (isset($page))
-        @edit('pages', $page->id)
-    @endif
-@endsection
+</body>
+
+</html>
